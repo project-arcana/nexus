974a4f3bbe37ccb385dc100954f5f9bd237171bf
Philip Trettner <Philip.Trettner@rwth-aachen.de>
2023-05-28 13:22:44 +0200

    fixed nullptr to_string

--- src/nexus/detail/make_string_repr.hh
+++ src/nexus/detail/make_string_repr.hh
@@ -13,7 +13,9 @@ template <class T>
 cc::string make_string_repr(T const& v)
 {
     cc::string s;
-    if constexpr (std::is_constructible_v<cc::string, T const&>)
+    if constexpr (std::is_same_v<T, decltype(nullptr)>)
+        s = "[nullptr]";
+    else if constexpr (std::is_constructible_v<cc::string, T const&>)
         s = '"' + cc::string(v) + '"';
 #ifdef NX_HAS_REFLECTOR
     // TODO: some basic to_string in case reflector is not available
